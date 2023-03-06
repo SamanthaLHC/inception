638447c64a36675c2df09083d542314907294f8b
sle-huec <s.lhuec@gmail.com>
2023-03-06 11:58:19 +0100

    script wordpress

--- srcs/requirements/wordpress/wp_conf.sh
+++ srcs/requirements/wordpress/wp_conf.sh
@@ -1,19 +1,30 @@
 sleep 10
 
-# si pas de wp-config.php do : 
-wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+if [! -a wp-config.php]
+then
+	wp config create	--allow-root \
+						--dbname=$SQL_DATABASE \
+						--dbuser=$SQL_USER \
+						--dbpass=$SQL_PASSWORD \
+						--dbhost=mariadb:3306 \ 
+						--path='/var/www/wordpress'
 
-# todo  configurer la pas automatiquement 			
-wp core install
-# todo  configurer la pas automatiquement creer un utilisateur
-wp user create
+# configurer la page automatiquement 			
+	wp core install --url=$WP_SITE \
+					--title=$WP_TITLE \
+					--admin_user=$WP_AMIN \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--admin_email=$WP_ADMIN_EMAIL \
 
-# si pas de dossier le creer:
-mkdir -p /run/php
+# creer un second utilisateur
+	wp user create
+fi
 
+if [! -d /run/php]
+then 
+	mkdir -p /run/php
+fi
+
+# run php-fpm:
 /usr/sbin/php-fpm7.3 -F
 
