fd1170a9829b699fac0018b57f37cf117ecea071
sle-huec <s.lhuec@gmail.com>
2023-03-06 15:48:48 +0100

    correctif dockerfile et script wp/ init docker-compose.yml

--- srcs/requirements/wordpress/wp_conf.sh
+++ srcs/requirements/wordpress/wp_conf.sh
@@ -1,30 +1,38 @@
+#!/bin/bash
+
 sleep 10
 
-if [! -a wp-config.php]
-then
-	wp config create	--allow-root \
-						--dbname=$SQL_DATABASE \
-						--dbuser=$SQL_USER \
-						--dbpass=$SQL_PASSWORD \
-						--dbhost=mariadb:3306 \ 
-						--path='/var/www/wordpress'
+if [ ! -a wp-config.php ]; then
+	wp config create --allow-root \
+		--dbname=$SQL_DATABASE \
+		--dbuser=$SQL_USER \
+		--dbpass=$SQL_PASSWORD \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/wordpress'
 
-# configurer la page automatiquement 			
-	wp core install --url=$WP_SITE \
-					--title=$WP_TITLE \
-					--admin_user=$WP_AMIN \
-					--admin_password=$WP_ADMIN_PASSWORD \
-					--admin_email=$WP_ADMIN_EMAIL \
+	# configurer la page automatiquement
+	wp core install --allow-root \
+		--url=$WP_SITE \
+		--title=$WP_TITLE \
+		--admin_user=$WP_AMIN \
+		--admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--path='/var/www/wordpress' 
 
-# creer un second utilisateur
-	wp user create
+	# creer un second utilisateur
+	wp user create --allow-root \
+		--user-login=$WP_USER \
+		--user-email=$WP_USER_EMAIL \
+		--role=$USER_ROLE \
+		--user_pass=$WP_USER_PASSWORD \
+		--admin_email=$WP_USER_EMAIL \
+		--path='/var/www/wordpress' 
+
 fi
 
-if [! -d /run/php]
-then 
+if [ ! -d /run/php ]; then
 	mkdir -p /run/php
 fi
 
 # run php-fpm:
 /usr/sbin/php-fpm7.3 -F
-
