e54bd50eea0f7b4ff5988fc9cf5a7e8036ddafe1
Samantha Le Huec <s.lhuec@gmail.com>
2023-03-10 02:27:15 +0100

    add sleep in script

--- srcs/requirements/mariadb/launch_mdb.sh
+++ srcs/requirements/mariadb/launch_mdb.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
+set -x
 
 if [ ! -f "/var/lib/mysql/file_flag_mdb_done" ]; then 
 	chown -R mysql:mysql /var/lib/mysql
 	mkdir -p /var/run/mysqld
 	chown -R mysql:mysql /var/run/mysqld
-
+	
 	service mysql start
 
-	mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-	mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-	mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-	mysql -e "FLUSH PRIVILEGES;"
-
+	# Wait for service to be ready
+	echo -n "Waiting for service to be fully started"
+	sleep 1 # Sleep at least once
+	while [ ! -e /var/run/mysqld/mysqld.sock ]; do 
+		echo -n .
+		sleep 1
+	done
+	echo ""
+	
+	mysql << EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS \`${SQL_USER}\`@\`localhost\` IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+	
+	sleep 5
 	mysqladmin -uroot -p$SQL_ROOT_PASSWORD shutdown
 	touch /var/lib/mysql/file_flag_mdb_done
 fi
 
-exec mysqld_safe
+set +x
+exec mysqld
