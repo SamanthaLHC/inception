a60e85536e3917621a2f26b68fc16507f5540773
Samantha Le Huec <s.lhuec@gmail.com>
2023-03-19 15:08:34 +0100

    finish

--- fclean.sh
+++ fclean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop '$(docker ps -qa)'
+docker stop $(docker ps -qa)
 
-docker rm '$(docker ps -qa)' 
+docker rm $(docker ps -qa)
 
-docker rmi -f '$(docker images -qa)'
+docker rmi -f $(docker images -qa)
 
-docker volume rm '$(docker volume ls -q)'
+docker volume rm $(docker volume ls -q)
 
-docker network rm '$(docker network ls -q)' 2>/dev/null
+docker network rm $(docker network ls -q) 2>/dev/null
